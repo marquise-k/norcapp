105390248fd12ef9407f19249e280bc051b23aa3
grsmyth <grsmyth@deloitte.ca>
2019-09-03 12:56:04 -0400

    Moved search icon

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -7,7 +7,7 @@ class Searchbar extends Component {
       // eslint-disable-next-line react/jsx-filename-extension
       <div className="App">
         <form className="search-form">
-          <a href="/Dashboard">
+          <a href="/Dashboard" className="search-button">
             <img
               className="search-icon search-items"
               src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"
