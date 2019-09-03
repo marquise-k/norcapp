881ab76f7460689750d45e7e138fbb8c4e215ce5
grsmyth <grsmyth@deloitte.ca>
2019-09-03 11:45:40 -0400

    Saving my stuff

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import '../styles.scss';
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
-// require('./styles.scss');
+import React, { useEffect, useState, Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+import Cards from '../components/Cards';
+// import firebase from '../components/Firebase';
+import Searchbar from '../components/Searchbar';
+import Sidebar from '../components/Sidebar';
+import './App.css';
+import { loadFirebase } from '../lib/database';
 
-function Home() {
-  return <App />;
-}
+const App = (props) => {
+  console.log(props);
 
-export default Home;
+  return (
+    <div>
+      <Searchbar />
+      {props.cards &&
+        props.cards.map(card => (
+          <Cards
+            key={card.id}
+            title={card.title}
+            start_time={card.start_time}
+            end_time={card.end_time}
+          />
+        ))}
+      <Sidebar />
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// eslint-disable-next-line func-names
+App.getInitialProps = async function () {
+  const firebase = await loadFirebase();
+
+  const result = await new Promise((resolve, reject) => {
+    firebase
+      .firestore()
+      .collection('cards')
+      .limit(10)
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot);
+        const data = [];
+        snapshot.forEach((doc) => {
+          data.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        console.log(data);
+        resolve(data);
+      })
+      .catch(() => {
+        reject(new Error('Something went wrong'));
+      });
+  });
+  console.log(result);
+  return { cards: result };
+};
+
+export default App;
