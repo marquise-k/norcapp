87d5266eb114698b38f1433a370646eef10c7123
grsmyth <grsmyth@deloitte.ca>
2019-09-03 12:48:27 -0400

    removing comment

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Searchbar extends Component {
