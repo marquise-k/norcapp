3f030d13581523bf27a97ff69b1b5e53744cbb94
Marquise Kamanke <mkamanke@deloitte.ca>
2019-09-03 13:49:28 -0400

    fixed cards structure

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,38 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default class extends Component {
-  render() {
-    console.log(this.props)
-    const { card } = this.props
-
-    return (
-      <div className="card-container">
-        <div className="wrapper" />
-        <div className="clash-card" />
-        {card && (
-          <div>
-            <img src="" alt="" />
-            <h4>{card.title}</h4>
-            <hr />
-            <h6>
-              {card.starttime} - {card.endtime}
-            </h6>
-            <h6>Date</h6>
-            <p />
-            <p>{card.Description}</p>
-          </div>
-        )}
-        {!card && <p>Have nothing</p>}
 const Cards = ({ card }) => (
-  <div>
+  <div className="card-container">
+    <div className="wrapper" />
+    <div className="clash-card" />
     {card && (
       <div>
         <img src="" alt="" />
         <h4>{card.title}</h4>
         <hr />
         <h6>
-          {card.start_time} - {card.end_time}
+          {card.starttime} - {card.endtime}
         </h6>
         <h6>Date</h6>
         <p />
@@ -41,15 +20,15 @@ const Cards = ({ card }) => (
     )}
     {!card && <p>Have nothing</p>}
   </div>
-);
+)
 
-export default Cards;
+export default Cards
 
 Cards.propTypes = {
   card: PropTypes.shape({
     title: PropTypes.string,
     start_time: PropTypes.string,
     end_time: PropTypes.string,
-    Description: PropTypes.string,
-  }).isRequired,
-};
+    Description: PropTypes.string
+  }).isRequired
+}
