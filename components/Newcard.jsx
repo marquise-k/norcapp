4e8034343066d01ce1a6c302acc537533dd2711a
Marquise Kamanke <mkamanke@deloitte.ca>
2019-09-04 09:56:53 -0400

    added new card component

--- components/Newcard.jsx
+++ components/Newcard.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import PlusButton from '../assets/plus-button.png'
+import PlusButton from '../assets/plusbutton.png'
 
 const NewCards = () => (
   <div className="card-container">
-    {card && (
-      <div className="wrapper">
-        <div className="clash-card">
-          <img className="plus-button" src={PlusButton} alt="The plus button" />
-          <h4 className="card-heading">Create New Event</h4>
-          <p />
-          <p className="card-description">
-            Don't see what you're looking for? Organize your own uniquee event
-            for your interest.
-          </p>
-        </div>
+    <div className="wrapper">
+      <div className="clash-card">
+        <img className="plus-button" src={PlusButton} alt="The plus button" />
+        <h4 className="card-heading">Create New Event</h4>
+        <p />
+        <p className="card-description">
+          Don't see what you're looking for? Organize your own uniquee event for
+          your interest.
+        </p>
       </div>
-    )}
-    {!card && <p>Have nothing</p>}
+    </div>
   </div>
 )
 
