0d3258e04dbeb511103dbc240330638639592971
grsmyth <grsmyth@deloitte.ca>
2019-09-04 11:33:04 -0400

    Added some semicolons

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Sidebar = () => (
   <div className="sidebar-items">
@@ -14,6 +14,6 @@ const Sidebar = () => (
       Directory
     </a>
   </div>
-)
+);
 
-export default Sidebar
+export default Sidebar;
