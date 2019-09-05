5f1499199b2f8c35b36e0594dbc2c1376b09de8a
grsmyth <grsmyth@deloitte.ca>
2019-09-05 14:14:21 -0400

    Card area newcard fix

--- components/CardArea.jsx
+++ components/CardArea.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from 'react'
-import PropTypes from 'prop-types'
-import Cards from './Cards'
-import NewCards from './NewCards'
-import { Context } from '../context/Context'
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import Cards from './Cards';
+import NewCards from './NewCards';
+import { Context } from '../context/Context';
 
 // const filteredCards = filteredCards.filter(eachItem => {
 //   return (
@@ -16,25 +16,25 @@ import { Context } from '../context/Context'
 // })
 
 const CardArea = ({ cards }) => {
-  const { searchText } = useContext(Context)
+  const { searchText } = useContext(Context);
   const [filteredCards, setFilterCards] = useState(
     cards.filter(
       card =>
         card.title.toLowerCase().includes(searchText.toLowerCase()) ||
         card.Description.toLowerCase().includes(searchText.toLowerCase())
     )
-  )
+  );
 
   return (
     <div className="card-area">
-      <NewCard />
+      <NewCards />
       {filteredCards &&
         filteredCards.map((card, index) => <Cards card={card} key={index} />)}
     </div>
-  )
-}
+  );
+};
 
-export default CardArea
+export default CardArea;
 
 CardArea.propTypes = {
   cards: PropTypes.arrayOf(
@@ -42,7 +42,7 @@ CardArea.propTypes = {
       title: PropTypes.string,
       start_time: PropTypes.string,
       end_time: PropTypes.string,
-      Description: PropTypes.string
+      Description: PropTypes.string,
     })
-  ).isRequired
-}
+  ).isRequired,
+};
