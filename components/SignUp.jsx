3b93cd3e207a4751e4ca1bcf6f91995f05416e4c
grsmyth <grsmyth@deloitte.ca>
2019-09-06 07:50:49 -0400

    Styling changes

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,30 +1,33 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import SignUpButton from '../assets/sign-up-button.png'
-import GoingButton from '../assets/going-button.png'
-import { Context } from '../context/Context'
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import SignUpButton from '../assets/sign-up-button.png';
+import GoingButton from '../assets/going-button.png';
+// import { Context } from '../context/Context';
 
 const SignUp = () => {
-  const { Going, setGoing } = useContext(Context)
+  // const { Going, setGoing } = useContext(Context)
+  const [Going, setGoing] = useState(false);
   const onClick = () => {
-    setGoing(!Going)
-  }
+    setGoing(!Going);
+  };
 
-  const ButtonArea = children => <div className="ButtonArea">{children}</div>
-
-  if (Going) {
-    return ButtonArea(
-      <button className="button-sign" type="button-sign" onClick={onClick}>
-        <img className="going-button" src={GoingButton} alt="A button" />{' '}
-      </button>
-    )
-  }
+  const ButtonArea = children => <div className="ButtonArea">{children}</div>;
 
   return ButtonArea(
-    <button className="button-sign" onClick={onClick}>
-      <img className="going-button" src={SignUpButton} alt="A button" />
+    <button className="button-sign" type="button" onClick={onClick}>
+      <img
+        className="going-button"
+        src={Going ? GoingButton : SignUpButton}
+        alt="A button"
+      />
     </button>
-  )
-}
+  );
 
-export default SignUp
+  // return ButtonArea(
+  //   <button className="button-sign" type="button" onClick={onClick}>
+  //     <img className="going-button" src={SignUpButton} alt="A button" />
+  //   </button>
+  // );
+};
+
+export default SignUp;
